485ccd316bfecc25ee44fbd691ffc27eea432fa1
konradjk <konradjkarczewski@gmail.com>
2017-09-12 18:25:23 +0900

    using chrom-pos-ref-alt for now

--- forms.py
+++ forms.py
@@ -9,10 +9,11 @@ CHROMOSOMES.extend(['chr{}'.format(x) for x in CHROMOSOMES])
 
 class SignupForm(FlaskForm):
     email = StringField(u'Your email address', validators=[Email(), DataRequired()])
-    chromosome = StringField(u'Chromosome', validators=[DataRequired(), AnyOf(CHROMOSOMES)])
-    position = IntegerField(u'Position', validators=[DataRequired()])
-    reference = StringField(u'Reference allele', validators=[DataRequired()])
-    alternate = StringField(u'Alternate allele', validators=[DataRequired()])
+    chr_pos_ref_alt = StringField(u'Chrom-Pos-Ref-Alt', validators=[DataRequired(), Regexp('([1[0-9]|2[0-2]|\d)-\d+-[ATCG]+-[ATCG]+')])
+    # chromosome = StringField(u'Chromosome', validators=[DataRequired(), AnyOf(CHROMOSOMES)])
+    # position = IntegerField(u'Position', validators=[DataRequired()])
+    # reference = StringField(u'Reference allele', validators=[DataRequired()])
+    # alternate = StringField(u'Alternate allele', validators=[DataRequired()])
 
     eula = StringField(u'I did not read the terms and conditions')
 
